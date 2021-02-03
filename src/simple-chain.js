0acd5724831d79cde436ccd2962774b8e1d0a983
Nursultan <65071183+Nursultan-coder@users.noreply.github.com>
2021-02-03 19:49:17 +0600

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  sChain : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.sChain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.sChain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof (position)!== 'number'){
+        this.sChain = [];
+        throw new Error();
+    }
+      if(position-1 < 0 || position-1 >= this.getLength()){
+          this.sChain = [];
+          throw new Error();
+      }
+    this.sChain.splice(--position,1);
+      return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.sChain.reverse();
+      return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chainString = '';
+    for(let i = 0;i<this.sChain.length;i++){
+      if(i<this.sChain.length-1 ){
+          chainString+= '( ' + this.sChain[i] + ' )' + '~~';
+      }
+      else{
+          chainString+= '( ' + this.sChain[i] + ' )';
+      }
+    }
+    this.sChain=[];
+    return chainString;
   }
 };
 
